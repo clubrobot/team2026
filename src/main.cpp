b75943f317f3c160c6482f5b72da4d1fdfbbdf3f
Pixelboris <pixelbo21@gmail.com>
2024-10-19 21:55:51 +0200

    STM32 de test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
+#include <HardwareTimer.h>
 
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
 }
 
+/**
+ * @brief Test
+ */
 void loop() {
-
-Serial.println("J'aime le Club Robot");
+    Serial.println("J'aime le Club Robot");
+    delay(100);
 }
