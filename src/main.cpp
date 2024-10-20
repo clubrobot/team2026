9db447734b275404265ebf5c76a45cea3e85a325
Pixelboris <pixelbo21@gmail.com>
2024-10-20 11:37:42 +0200

    Import des Libs + todo.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include <Arduino.h>
-#include <STM32FreeRTOS.h>
-#include <HardwareTimer.h>
 
-void setup() {
-// write your initialization code here
-    Serial.begin(9600);
+void setup(){
+
 }
 
-/**
- * @brief Test
- */
-void loop() {
-    Serial.println("J'aime le Club Robot");
-    delay(100);
+void loop(){
+
+
 }
